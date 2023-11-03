483f3da79a8bd67f3d09dae91d0c4a303c644292
fcorpus <fcastillocorpus@gmail.com>
2023-11-03 08:50:33 -0600

    Agregar archivos de proyecto.

---
+++ busqueda3Niveles/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace busqueda3Niveles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cuadroBusqueda = new System.Windows.Forms.TextBox();
+            this.Resultados = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cuadroBusqueda
+            // 
+            this.cuadroBusqueda.Location = new System.Drawing.Point(29, 36);
+            this.cuadroBusqueda.Name = "cuadroBusqueda";
+            this.cuadroBusqueda.Size = new System.Drawing.Size(100, 20);
+            this.cuadroBusqueda.TabIndex = 1;
+            // 
+            // Resultados
+            // 
+            this.Resultados.FormattingEnabled = true;
+            this.Resultados.Location = new System.Drawing.Point(29, 102);
+            this.Resultados.Name = "Resultados";
+            this.Resultados.Size = new System.Drawing.Size(353, 277);
+            this.Resultados.TabIndex = 2;
+            this.Resultados.Click += new System.EventHandler(this.Resultados_Click);
+            this.Resultados.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(409, 102);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(365, 277);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(162, 36);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Resultados);
+            this.Controls.Add(this.cuadroBusqueda);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox cuadroBusqueda;
+        private System.Windows.Forms.ListBox Resultados;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ busqueda3Niveles/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace busqueda3Niveles
+{
+    public partial class Form1 : Form
+    {
+        string texto;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Resultados.Items.Clear();
+            cargaListBox();
+        }
+
+        private void cargaListBox()
+        {
+            texto = cuadroBusqueda.Text;
+            if(cuadroBusqueda.Text.Contains(" "))
+            {
+                texto = cuadroBusqueda.Text.Replace(" ","-");
+            }
+            WebRequest request = WebRequest.Create("https://genius.com/artists/" + texto + "/albums");
+            Thread.Sleep(3000);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            string html = responseFromServer;
+            Regex r = new Regex("<h3 class=\"ListItem__Title-sc-122yj9e-4 nknYf\">(.*?)</h3>");
+            MatchCollection matches = r.Matches(html);
+
+            if (matches.Count > 0 )
+            {
+                foreach(Match match in matches)
+                {
+                    string artista = match.Groups[1].Value;
+                    Resultados.Items.Add(artista);
+                }
+            }
+        }
+
+        private void Resultados_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            if(Resultados.SelectedIndex != -1)
+            {
+                string album = Resultados.SelectedItem.ToString();
+                cargaTextBox(album);
+            }
+        }
+        
+        private void cargaTextBox(string album)
+        {
+            string nuevoAlbum = album;
+            if(album.Contains(" "))
+            {
+                nuevoAlbum = album.Replace(" ", "-");
+            }
+            WebRequest request = WebRequest.Create("https://genius.com/albums/" + texto + "/"+ nuevoAlbum);
+            Thread.Sleep(3000);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            string html = responseFromServer;
+            Regex r = new Regex("<h3 class=\"chart_row-content-title\">\\s*(.*?)\\s*<span");
+            MatchCollection matches = r.Matches(html);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    string cancion = match.Groups[1].Value;
+                    richTextBox1.AppendText(cancion);
+                    richTextBox1.AppendText("\n");
+                }
+            }
+        }
+    }
+}
